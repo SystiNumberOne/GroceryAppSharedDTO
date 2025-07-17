d345547af1151223dcd9295894807efbb32d4da0
SystiNumberOne <system.visualcontrol@icloud.com>
2025-07-17 22:32:44 +0200

    Added GroceryCategoryRequestDTO an ResponseDTO

---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
@@ -0,0 +1,20 @@
+//
+//  GroceryCategoryRequestDTO.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Systemintegration VisualControl on 17.07.25.
+//
+
+import Foundation
+
+public struct GroceryCategoryRequestDTO: Codable {
+    
+    public let title: String
+    public let colorCode: String
+    
+    public init(title: String, colorCode: String) {
+        self.title = title
+        self.colorCode = colorCode
+    }
+    
+}
---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  GroceryCategoryResponseDTO.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Systemintegration VisualControl on 17.07.25.
+//
+
+import Foundation
+
+public struct GroceryCategoryResponseDTO: Codable {
+    
+    public let id: UUID
+    public let title: String
+    public let colorCode: String
+    
+    public init(id: UUID, title: String, colorCode: String) {
+        self.id = id
+        self.title = title
+        self.colorCode = colorCode
+    }
+    
+}
